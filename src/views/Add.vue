1ae374be2ebfd1eac45e58833edd4ec6d4e38b2d
yuta-hayashi <dev.hyuta@gmail.com>
2020-05-24 13:47:50 +0900

    Add add-page.

---
+++ src/views/Add.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" placeholder="name" v-model="newName" />
+    <button @click="add">OK</button>
+    {{ newName }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { itemsModule } from "@/store/item";
+
+@Component
+export default class AddPage extends Vue {
+  newName = "";
+  add() {
+    itemsModule.addItem(this.newName, "imgUrl");
+    this.$router.push("/");
+  }
+}
+</script>
