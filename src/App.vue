84cf98cb20d4f3092a2a6c39253a631eca7afdfe
yuta-hayashi <dev.hyuta@gmail.com>
2020-05-28 16:24:00 +0900

    Add normalize.css.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
 </template>
 
 <script lang="ts">
+import "normalize.css";
+import "@/assets/globalStyle.css";
 import { Component, Vue } from "vue-property-decorator";
 import { userModule } from "@/store/user";
 import firebase from "@/firebase";
@@ -26,26 +28,3 @@ export default class App extends Vue {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
