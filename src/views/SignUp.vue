d00c4484fc682332634b76410f562aaf92cdf048
yuta-hayashi <dev.hyuta@gmail.com>
2020-06-22 11:34:17 +0900

    Add demo mode.

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="signUpForm">
-    <MessageBar v-if="message!=''">{{message}}</MessageBar>
+    <MessageBar v-if="message != ''">{{ message }}</MessageBar>
     <button class="button" @click="signUpGoogle">Googleでログイン</button>
-    <hr>
-    <input type="text" class="input" placeholder="Mail" v-model="email">
-    <input type="password" class="input" placeholder="Password" v-model="password">
-    <button class="button button_primary" @click="signUpMail">新規登録</button>
-    <button class="button button_secondary" @click="signInMail">ログイン</button>
+    <hr />
+    <input type="text" class="input" placeholder="Mail" v-model="email" />
+    <input
+      type="password"
+      class="input"
+      placeholder="Password"
+      v-model="password"
+    />
+    <div class="button_group">
+      <button class="button button_secondary login" @click="signInMail">
+        ログイン
+      </button>
+      <button class="button button_primary" @click="signUpMail">
+        新規登録
+      </button>
+    </div>
+    <button class="button" @click="demo">デモアカウント</button>
 
-    <button @click="$router.push('/about')" class="button ">このアプリについて</button>
+    <router-link to="/about">
+      このアプリについて
+    </router-link>
   </div>
 </template>
 
@@ -16,44 +30,48 @@
 // @ is an alias to /src
 import { Component, Vue } from "vue-property-decorator";
 import { userModule } from "@/store/user";
-import firebase from "@/firebase"
-import MessageBar from "@/components/MessageBar.vue"
+import firebase from "@/firebase";
+import MessageBar from "@/components/MessageBar.vue";
 
 @Component({
-  components:{
+  components: {
     MessageBar
   }
 })
 export default class Home extends Vue {
-  message="";
-  email="";
-  password="";
+  message = "";
+  email = "";
+  password = "";
   mounted() {
     document.title = `Likes- SingUp`;
   }
-  signUpMail(){
-    firebase.auth().createUserWithEmailAndPassword(this.email,this.password)
-    .then( (obj)=>{
-      if(obj.user!==null){
-        this.$router.push("/");
-      }
-    }
-    )
-    .catch((error)=>{
-      this.message=error.message;
-      console.error(error.message)});
+  signUpMail() {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.email, this.password)
+      .then((obj) => {
+        if (obj.user !== null) {
+          this.$router.push("/");
+        }
+      })
+      .catch((error) => {
+        this.message = error.message;
+        console.error(error.message);
+      });
   }
-  signInMail(){
-    firebase.auth().signInWithEmailAndPassword(this.email,this.password)
-    .then( (obj)=>{
-      if(obj.user!==null){
-        this.$router.push("/");
-      }
-    }
-    )
-    .catch((error)=>{
-      this.message=error.message;
-      console.error(error.message)});
+  signInMail() {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.email, this.password)
+      .then((obj) => {
+        if (obj.user !== null) {
+          this.$router.push("/");
+        }
+      })
+      .catch((error) => {
+        this.message = error.message;
+        console.error(error.message);
+      });
   }
   signUpGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -66,16 +84,24 @@ export default class Home extends Vue {
         }
       })
       .catch((error) => {
-        this.message=error.message;
-        console.error(error.message)});
+        this.message = error.message;
+        console.error(error.message);
+      });
+  }
+  demo() {
+    this.email = "demo@yuta.run";
+    this.password = "demo@yuta.run";
+    this.signInMail();
   }
 }
 </script>
 
 <style scoped>
-#signUpForm{
+#signUpForm {
   display: flex;
   flex-direction: column;
-  display: flex;
+}
+.login {
+  margin-right: 2em;
 }
 </style>
